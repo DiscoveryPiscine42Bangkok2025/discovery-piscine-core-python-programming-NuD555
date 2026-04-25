d4802ce39dfdbff96ff71e012235bd8c2ee6eec2
NuD555 <fencai74@gmail.com>
2026-04-25 14:50:52 +0700

    Add files via upload

---
+++ cell02/ex02/password.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+password_storage = "Python is awesome"
+
+user_input = input()
+
+if user_input == password_storage:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
