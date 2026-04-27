a231a0f6f4fb84f32a9901e126f95aa90559546a
NuD555 <fencai74@gmail.com>
2026-04-27 11:10:10 +0700

    Add files via upload

--- cell04/ex00/upcase_it.py
+++ cell04/ex00/upcase_it.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
-word = input("Give me a word: ")
 
-print(word.upper())
+def main():
+    word = input("Give me a word: ")
+    
+    print(word.upper())
+
+if __name__ == "__main__":
+    main()
